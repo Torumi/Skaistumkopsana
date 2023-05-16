72fad36c2be7a910b574c4c5678a167545f864dd
Torumi <iljacap@gmail.com>
2023-05-16 23:35:35 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,24 +1,72 @@
-import tkinter as tk
-from tkinter import ttk
-
-window = tk.Tk()
-window.title("Skaistumkopsana")
-window.geometry('1920x1080')
+import datetime
 
 
-new_client_frame = ttk.Frame(borderwidth=1, relief='solid', padding=[10,10])
-name_label = tk.Label(new_client_frame, text='Name')
-name_entry = ttk.Entry(new_client_frame)
-surname_label = tk.Label(new_client_frame, text='Surname')
-surname_entry = ttk.Entry(new_client_frame)
+class Time(datetime.time):
 
-name_label.pack(anchor="nw")
-name_entry.pack(anchor='nw')
-surname_label.pack(anchor="nw")
-surname_entry.pack(anchor='nw')
-
-new_client_frame.pack(anchor='nw')
+    def __sub__(self, other):
+        return datetime.timedelta(hours=self.hour - other.hour, minutes=self.minute - other.minute,
+                                  seconds=self.second - other.second)
 
 
+class Client:
 
-window.mainloop()
+    def __init__(self, name: str, surname: str, personal_code: str, phone_number: str | int):
+        self.name = name
+        self.surname = surname
+        self.personal_code = personal_code
+        self.phone_number = phone_number
+
+    def __str__(self):
+        return f"{self.name} {self.surname} ({self.personal_code}, {self.phone_number})"
+
+
+class BeautyProcedure:
+
+    def __init__(self, category: str = None, name: str = None, discount: str = None, price: int | float = None,
+                 client: Client = None, date: datetime.date = None, start_time: Time = None,
+                 end_time: Time = None, time_isable: bool = True):
+        self.category = category
+        self.name = name
+        self.discount = discount
+        self.price = price
+        self.client = client
+        self.date = date
+        self.start_time = start_time
+        self.end_time = end_time
+        self.time_isable = time_isable
+
+    @property
+    def duration(self):
+        if self.start_time and self.end_time:
+            return self.end_time - self.start_time
+
+    @property
+    def total_price(self):
+        if self.discount and self.price:
+            discount = int(self.discount.split('%')[0]) / 100
+            return self.price - self.price * discount
+
+    def info(self):
+        return (f"Category: {self.category}\n"
+                f"Name: {self.name}\n"
+                f"Client: {str(self.client)}\n"
+                f"Date: {self.date.strftime('%d.%m.%Y')}"
+                f" from {self.start_time.strftime('%H:%M')} to {self.end_time.strftime('%H:%M')}\n"
+                f"Duration: {self.duration}\n"
+                f"Price: {self.price}\n"
+                f"Discount: {self.discount}\n"
+                f"Total price: {self.total_price}\n")
+
+    def print_info(self):
+        print(self.info())
+
+    def client_info(self):
+        return str(self.client)
+
+    def print_client_info(self):
+        print(self.client_info())
+
+
+c1 = BeautyProcedure('a', 'b', '10%', 40, Client('ila', 'Chap', '281005-21423', '27823867'), datetime.date.today(),
+                     Time(20, 47), Time(23, 00))
+c1.print_info()
